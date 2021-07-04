48b5584fcbe00e8103108d6cfc965e7d43292b97
sszumal@gmail.com <sszumal@gmail.com>
2021-07-04 13:40:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmartResourceMonitor_MSZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
